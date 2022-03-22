b9531e44dd1e04c089846a41cbeed0f8ea24c6d3
Andrey Astrovzorov <astrokoit@hotmail.com>
2022-03-23 01:46:26 +0700

    Добавьте файлы проекта.

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows;
+using Prism.Mvvm;
+
+namespace RSA_App.ViewModel
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        MainWindowViewModel()
+        {
+
+        }
+    }
+}
